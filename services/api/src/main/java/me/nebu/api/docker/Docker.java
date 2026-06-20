2b1311d54f1a26b6be55112af5ae6ecc4b4b549b
Nebulations <thoczit@gmail.com>
2026-06-19 21:39:33 -0400

    second docker test

--- services/api/src/main/java/me/nebu/api/docker/Docker.java
+++ services/api/src/main/java/me/nebu/api/docker/Docker.java
@@ -1,11 +1,29 @@
 package me.nebu.api.docker;
 
 import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.DockerClientConfig;
+import com.github.dockerjava.core.DockerClientImpl;
+import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
+import com.github.dockerjava.transport.DockerHttpClient;
+
+import java.time.Duration;
 
 public class Docker {
 
-    private static final DockerClient client = DockerClientBuilder.getInstance().build();
+    private static final DockerClientConfig config =
+            DefaultDockerClientConfig.createDefaultConfigBuilder()
+                    .build();
+
+    private static final DockerHttpClient httpClient =
+            new ApacheDockerHttpClient.Builder()
+                    .dockerHost(config.getDockerHost())
+                    .sslConfig(config.getSSLConfig())
+                    .build();
+
+    private static final DockerClient client =
+            DockerClientImpl.getInstance(config, httpClient);
 
     public static DockerClient getClient() {
         return client;
